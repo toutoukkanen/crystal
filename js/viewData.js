3c939edb361c35197e26fdbcd567080520095605
toutoukkanen <tuukka.007@hotmail.com>
2020-04-27 21:05:47 +0300

    First prototype of AJAX document listing

--- js/viewData.js
+++ js/viewData.js
@@ -1,26 +1,43 @@
 'use strict';
 
-console.log('skripti alkaa');
-const xhr = new XMLHttpRequest();
-xhr.open('get', 'https://raw.githubusercontent.com/toutoukkanen/crystal/master/documents/level1_incident_report7.html', true);                // Kerrotaan XMLHttpRequest-oliolle metodi ja osoite, johon pyyntö lähetetään sekä vaihdetaan toiminta synkroniseksi (true)
-xhr.onreadystatechange = printLink;                         // Kuunnellaan muutoksia latauksen tilassa, ja aina kun muutoksia tapahtuu ajetaan naytaKuva-funktio
-xhr.send(null);                                             // Lähetetään pyyntö     
+// TODO:
+// Add clearance check
 
-function printLink() 
-{                    
-  if (xhr.readyState === 4 && xhr.status === 200) 
-  {         
-    // Create a clone file of the raw HTML
-    var clonePage = document.createElement('html');
-    clonePage.innerHTML = xhr.responseText;
+function getLevel1Data(url)
+{
+    const xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);                // Kerrotaan XMLHttpRequest-oliolle metodi ja osoite, johon pyyntö lähetetään sekä vaihdetaan toiminta synkroniseksi (true)
+    xhr.onreadystatechange = printLink;                         // Kuunnellaan muutoksia latauksen tilassa, ja aina kun muutoksia tapahtuu ajetaan naytaKuva-funktio
+    xhr.send(null);                                             // Lähetetään pyyntö     
+    
+    function printLink() 
+    {                    
+      if (xhr.readyState === 4 && xhr.status === 200) 
+      {         
+        // Create a clone file of the raw HTML
+        var clonePage = document.createElement('html');
+        clonePage.innerHTML = xhr.responseText;
+    
+        var level1List = document.getElementById('level1');
+    
+        var li = document.createElement('li');
+    
+        var a = document.createElement('a');
+        li.appendChild(a);
 
-    //var p = document.querySelector('p');
-    //p.innerHTML = clonePage.querySelector('p').innerHTML;
+        let parsedURL = url.split('/'); // Divide the path in parts
+        a.href = 'documents/' + parsedURL[parsedURL.length - 1]; // Assign the local link as the href
+        a.innerHTML = clonePage.querySelector('title').innerHTML; // Find the title to name the link
 
-
-
-    console.log('asynkroninen lataus valmis');
-  }
+        level1List.appendChild(li); 
+    
+      }
+    }
 }
 
-console.log('skripti päättyy');
+getLevel1Data('https://raw.githubusercontent.com/toutoukkanen/crystal/master/documents/level1_incident_report7.html');
+getLevel1Data('https://raw.githubusercontent.com/toutoukkanen/crystal/master/documents/level1_regarding_your_job.html');
+
+
+
+
